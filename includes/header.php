488d364b97705d31d2634e729d5b0609254a401a
arieljarovisky <78839927+arieljarovisky@users.noreply.github.com>
2021-12-01 18:33:54 -0300

    Add files via upload

--- includes/header.php
+++ includes/header.php
@@ -26,27 +26,24 @@ header('location: iniciosesion.php');
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand" href="index.php">Verifarma</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="../../verifarma/index.php">Home</a>
+                        <a class="nav-link active" aria-current="page" href="../../verifarma/index.php">Inicio</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="../../verifarma/crearFarmacia.php">Crear Farmacia</a>
+                        <a class="nav-link" href="../../verifarma/create-pharmacy.php">Crear Farmacia</a>
                     </li>
                     
                     <li class="nav-item">
-                        <a class="nav-link" href="../../verifarma/cerrarSesion.php">Cerrar Sesion</a>
+                        <a class="nav-link" href="../../verifarma/logout.php">Cerrar Sesion</a>
                     </li>
                 </ul>
-                <form class="d-flex">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
+    
             </div>
         </div>
     </nav>
